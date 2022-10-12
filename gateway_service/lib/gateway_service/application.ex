cd061085dcd43f26073e8ffd2b409b38021291ef
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2022-10-12 18:16:47 +0300

    gateway forward request attempt

--- gateway_service/lib/gateway_service/application.ex
+++ gateway_service/lib/gateway_service/application.ex
@@ -8,10 +8,14 @@ defmodule GatewayService.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: GatewayService.Worker.start_link(arg)
-      # {GatewayService.Worker, arg}
+      {
+        Plug.Cowboy,
+        scheme: :http,
+        plug: GatewayService.Router,
+        options: [port: Application.get_env(:gateway_service, :port)
+        ]
+      }
     ]
-
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: GatewayService.Supervisor]
