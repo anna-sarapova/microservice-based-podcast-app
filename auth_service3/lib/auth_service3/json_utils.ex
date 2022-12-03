2fed4a270452c90e1280b6c9d9de59b9e55101d5
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2022-12-03 16:15:35 +0200

    fixed auth_service instances errors

---
+++ auth_service3/lib/auth_service3/json_utils.ex
@@ -0,0 +1,27 @@
+defmodule AuthService3.JSONUtils do
+  @moduledoc """
+  JSON Utilities
+  """
+
+  @doc """
+  Extend BSON to encode MongoDB ObjectIds to string
+  """
+  # Defining an implementation for the Json.Encode for BSON.ObjectId
+  defimpl Jason.Encoder, for: BSON.ObjectId do
+    # Implementing a custom encode function
+    def encode(id, options) do
+      # Converting the binary id to a string
+      BSON.ObjectId.encode!(id)
+      # Encoding the string to JSON
+      |> Jason.Encoder.encode(options)
+    end
+
+    def normaliseMongoId(document) do
+      document
+      # Set the id property to the value if _id
+      |> Map.put('id', document["_id"])
+      # Delete the _id property
+      |> Map.delete("_id")
+    end
+  end
+end
--- auth_service3/lib/auth_service3/register_plug.ex
+++ auth_service3/lib/auth_service3/register_plug.ex
@@ -3,28 +3,21 @@ defmodule AuthService3.RegisterPlug do
   require Logger
 
   def register_user(conn) do
-#    {:ok, timer_pid} = AuthService3.TaskTimeout.start_link(3000, conn)
     case conn.body_params do
       %{"name" => name, "email" => email, "password" => password} ->
         case Mongo.insert_one(:mongo, "Users", %{"name" => name, "email" => email, "password" => password}) do
           {:ok, user} ->
             Logger.info("received #{inspect(user)}", ansi_color: :light_magenta)
             document = Mongo.find_one(:mongo, "Users", %{_id: user.inserted_id})
-            #            response =
-            #              JSON.normaliseMongoId(document)
-            #              |> Jason.encode!()
             response = Jason.encode!(document)
-#            AuthService3.TaskTimeout.stop_timer(timer_pid)
             conn
             # Sets the value of the "content-type" response header
             |> put_resp_content_type("application/json")
             |> send_resp(200, response)
           {:error, _} ->
-#            AuthService3.TaskTimeout.stop_timer(timer_pid)
             send_resp(conn, 500, "Something went wrong")
         end
       _ ->
-#        AuthService3.TaskTimeout.stop_timer(timer_pid)
         send_resp(conn, 400, '')
     end
   end
